84f267f3bead31910fc7446c63f77f9599842667
Tim Jarratt <tjarratt@gmail.com>
2014-11-04 21:32:55 -0800

    Cleanup Euler1 swift implementation

--- swift/Euler/Euler/1.playground/section-1.swift
+++ swift/Euler/Euler/1.playground/section-1.swift
@@ -10,20 +10,10 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 var sum: Int
 sum = 0
-for n in 1..1000 {
+for n in 1 ..< 1000 {
     if n % 3 == 0 || n % 5 == 0 {
         sum += n
     }
 }
 
-sum
-
-sum = 0
-for var n = 3; n < 1000; n += 2 {
-    if n % 3 == 0 || n % 5 == 0 {
-        sum += n
-    }
-
-    n += 2
-}
 sum
